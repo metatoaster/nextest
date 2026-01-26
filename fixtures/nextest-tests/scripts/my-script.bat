da3999284fcf6c448f7e2ae52a2bf50b630fab85
Tommy Yu <y@metatoaster.com>
2026-01-27 02:19:09 +1300

    [nextest-runner] add setup scripts env vars
    Allow specific environment variables be defined for a given setup script
    such that they don't need to be wrapped by platform specific scripts
    (i.e. batch file vs. bash) to set them up when they may be platform
    agnostic.
    
    This also has the effect that allows specific environment variables be
    passed to specific tests via a simple wrapper program that may be
    configured directly on the nextest configuration.

--- fixtures/nextest-tests/scripts/my-script.bat
+++ fixtures/nextest-tests/scripts/my-script.bat
@@ -4,7 +4,12 @@ if defined __NEXTEST_SETUP_SCRIPT_ERROR exit 1
 REM Check that NEXTEST_PROFILE is set.
 if not defined NEXTEST_PROFILE exit 2
 
-ECHO MY_ENV_VAR=my-env-var>> %NEXTEST_ENV%
+if defined MY_ENV_VAR (
+    ECHO MY_ENV_VAR=%MY_ENV_VAR%>> %NEXTEST_ENV%
+    ECHO SCRIPT_NEXTEST=%NEXTEST%>> %NEXTEST_ENV%
+) else (
+    ECHO MY_ENV_VAR=my-env-var>> %NEXTEST_ENV%
+)
 ECHO SCRIPT_NEXTEST_PROFILE=%NEXTEST_PROFILE%>> %NEXTEST_ENV%
 
 REM If this environment variable is set, write a NEXTEST-prefixed env var.
